583afc001ae21a8828e85e2e8080aa247e72978b
prog3 <prog3@mail@gmail.com>
2020-03-30 10:34:49 +0300

    nominatim

--- index.js
+++ index.js
@@ -7,45 +7,56 @@ const resultsRead = [];
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-function geocoding(value) {
-  return axios.get('https://api', {
-    params: {
-      text: value,
-      limit: 1,
-    },
-    httpsAgent: new https.Agent({
-      rejectUnauthorized: false,
-    }),
-    timeout: 60000,
-  });
+async function geocoding(query) {
+  if (query.length > 0) {
+    const response = await axios.get(
+      'https://nominatim.openstreetmap.org/search',
+      {
+        params: {
+          format: 'json',
+          addressdetails: '0',
+          limit: 1,
+          q: query.join(' '),
+        },
+        // httpsAgent: new https.Agent({
+        //   rejectUnauthorized: false,
+        // }),
+        timeout: 10000,
+      }
+    );
+
+    let result = null;
+    const { data } = response;
+    if (data.length === 0) {
+      result = geocoding(query.slice(0, -1));
+    } else {
+      result = response;
+    }
+    return result;
+  }
+  return {};
 }
 
-fs.createReadStream('false-2-ok.csv')
-  .pipe(csv())
+console.log('...Start');
+
+fs.createReadStream('csv.csv')
+  .pipe(csv({ separator: ';' }))
   .on('data', data => resultsRead.push(data))
   .on('end', () => {
     const getData = () =>
       Promise.all(
         resultsRead.map(async (item, index) => {
-          await sleep(index * 1000);
-
           try {
-            let lat = null;
-            let lng = null;
+            await sleep(index * 50);
 
-            const response = await geocoding(item['Full Address']);
-
-            const { geo_centroid } = response.data || {};
-            console.log(geo_centroid);
-            if (geo_centroid) {
-              lat = geo_centroid.coordinates[1];
-              lng = geo_centroid.coordinates[0];
-            }
-
+            const query = item['Address'].split(' ').filter(i => i !== '');
+            const { data = [] } = await geocoding(query);
+            const { lat, lon } = data[0] || {};
+            // console.log(item['﻿№'], lat, lon);
             return {
               ...item,
               lat,
-              lng,
+              lon,
             };
           } catch (error) {
             console.log(error.message);
@@ -55,25 +66,30 @@ fs.createReadStream('false-2-ok.csv')
       );
 
     getData().then(data => {
-      console.log(data);
-
       const csvWriter = createCsvWriter({
-        path: 'file.csv',
+        path: 'geocoding.csv',
         header: [
-          { id: '№ объекта', title: '№ объекта' },
-          { id: 'Адрес объекта', title: 'Адрес объекта' },
-          { id: 'Город', title: 'Город' },
-          { id: 'Статус', title: 'Статус' },
-          { id: 'Region', title: 'Region' },
-          { id: 'Locality', title: 'Locality' },
+          { id: '﻿№', title: '№' },
+          { id: 'ПІБ', title: 'ПІБ' },
+          { id: 'ПІБ (лат.)', title: 'ПІБ (лат.)' },
+          { id: 'Стать', title: 'Стать' },
+          { id: 'Громадянство', title: 'Громадянство' },
+          { id: 'Серія, № паспорта', title: 'Серія, № паспорта' },
+          { id: 'Дата народження', title: 'Дата народження' },
+          { id: 'Назва приймаючої сторони', title: 'Назва приймаючої сторони' },
+          {
+            id: 'Адреса приймаючої сторони',
+            title: 'Адреса приймаючої сторони',
+          },
+          { id: 'Діти', title: 'Діти' },
+          { id: 'Ким введено інформацію', title: 'Ким введено інформацію' },
+          { id: 'Phone', title: 'Phone' },
           { id: 'Address', title: 'Address' },
-          { id: 'Full Address', title: 'Full Address' },
-          { id: 'Latitude', title: 'Latitude' },
-          { id: 'Longitude', title: 'Longitude' },
-          { id: 'lat', title: 'Lat' },
-          { id: 'lng', title: 'Lng' },
+          { id: 'lat', title: 'Latitude' },
+          { id: 'lon', title: 'Longitude' },
           { id: 'error', title: 'Error' },
         ],
+        // fieldDelimiter: ';',
       });
 
       csvWriter.writeRecords(data).then(() => {
